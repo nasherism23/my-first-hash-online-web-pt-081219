c19222dc52dada0277a36579ffe734b5edef8f4d
James Nash <nash.jamesm@gmail.com>
2019-08-31 16:09:32 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,9 +15,6 @@ def retrieval
                   "oil paintings" => 3
                  }
   shipping_manifest["oil paintings"]
-  
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
 end
 
 def adding
@@ -27,13 +24,13 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+  shipping_manifest["muskets"] = 2 
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+  shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
-
+shipping_manifest
 end
